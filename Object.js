e36d4a64c48aa0d7542ceff23bf48bba9b6d1cda
Stephen Swaroop Raj Gudipalli <Swaroop@Stephens-MacBook-Pro.local>
2017-09-19 16:25:50 -0500

    new file test

---
+++ Object.js
@@ -0,0 +1,8 @@
+function person(name, age) {
+		this.name = name;
+    this.age = age;
+}
+		var swaroop = new person ("Swaroop Raj ", 25);
+    var seshi = new person ("Seshendra T", 26);
+   console.log(swaroop.name + "'s age is " + swaroop.age);
+   console.log(seshi.name+ "'s age is "+ seshi.age);
